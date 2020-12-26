ba8d277925cf491c8f8afdfdd04e9d8b66d3df7a
Muhamad Ridwan Nullah <73370645+RidwanNullah@users.noreply.github.com>
2020-12-26 17:13:57 +0700

    Add files via upload

---
+++ tugas4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class tugas4 {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(" ");
+
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = i; j <= n; j++) {
+
+                System.out.println(j);
+
+            }
+            }
+
+
+
+        }
+
+    }
